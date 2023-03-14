9574d0b8786aaf68d8bcf3ec4f1c45709a5be2a7
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2023-03-14 08:32:20 -0400

    Adding login UI work

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -4,18 +4,18 @@ import './Login.css'
 export default function Login() {
 
     return (
-        <form>
-            <div className="login-container">
-                <label className="username"><strong>Username</strong></label><br />
-                <input type="text" placeholder="Enter Username" className='username-input' required /><br />
-                <label className="password"><strong>Password</strong></label><br />
-                <input type="password" placeholder="Enter Password" className="password-input" required /><br />
-                <button className='login-button' type="submit">Login</button>
-                <label>
-                    <input type="checkbox" className='remember-me-input' />
-                    Remember Me
-                </label>
-            </div>
-        </form>
+        <div className="login-container">
+            <label className="username"><strong>Username:</strong></label><br />
+            <input type="text" placeholder="Enter Username" className='username-input' required /><br />
+            <label className="password"><strong>Password:</strong></label><br />
+            <input type="password" placeholder="Enter Password" className="password-input" required /><br />
+            <button className='login-button' type="submit">Login</button>
+            <label className="login-remember-me">
+                <input type="checkbox" className='remember-me-input' />
+                Remember Me
+            </label><br />
+            <button className="cancle-button">Cancle</button>
+            <label className="forgot-password-link"><a href="facebook.com">Forgot password?</a></label>
+        </div>
     );
 }
